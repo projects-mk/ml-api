01a31de93be8475a7ee31aea807f0edc24eea776
Mateusz <mateuszkurdziel1@gmail.com>
2024-05-06 11:58:39 +0200

    updated deployment and svc

--- kubernetes/create_secrets_and_deploy.sh
+++ kubernetes/create_secrets_and_deploy.sh
@@ -3,8 +3,8 @@ DOCKERHUB_TOKEN=$2
 NAMESPACE=ml-api
 APP_NAME=ml-api
 
-kubectl delete $APP_NAME-deployment -deployment -n $NAMESPACE 
-kubectl delete svc $APP_NAME-service -n $NAMESPACE
+kubectl delete services --all -n $NAMESPACE --ignore-not-found
+kubectl delete deployments --all -n $NAMESPACE --ignore-not-found
 
 kubectl create secret docker-registry gitea-creds \
     --docker-server=registry-c6af1e1a-8717-4821-9c1f-895998bf4e9c.cytr.us \
@@ -12,8 +12,6 @@ kubectl create secret docker-registry gitea-creds \
     --docker-password=$DOCKERHUB_TOKEN \
     --namespace=$NAMESPACE \
     --dry-run=client -o yaml | kubectl apply -f -
-
-
 
 kubectl apply -f deployment.yaml -n $NAMESPACE --wait
 kubectl apply -f svc.yaml -n $NAMESPACE --wait
